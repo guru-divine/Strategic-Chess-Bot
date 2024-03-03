789ee535f7f96f0c57bebda32a13c8395a3e9c83
Divya Raj <divya.raj@kgpian.iitkgp.ac.in>
2024-03-03 21:13:56 +0530

    Generated movement of pieces on chess board on user input

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def loadImages():
     pieces = ["bR", "bN", "bB", "bQ", "bK", "wR", "wN", "wB", "wQ", "wK", "bp", "wp"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
-        # IMAGES[piece] = p.transform.scale(p.image.load("img/" + piece + ".svg"), (SQ_SIZE, SQ_SIZE))
 
 
 
@@ -25,10 +24,33 @@ def main():
     # print(gs.board)
     loadImages() #load once
     running = True
+    sqSelected = () #keep track of last click of the user (6,4)
+    playerClicks = [] #keep track of player clicks: [(6,4), (4,4)]
     while(running):
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #(x, y) of cursor
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): #user clicked the same square twice
+                    sqSelected = () #deselect
+                    playerClicks = [] #clear player clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if(len(playerClicks) == 2): #after 2nd click
+                    if gs.board[playerClicks[0][0]][playerClicks[0][1]] == "--": #first click is on empty square
+                        sqSelected = ()  # Reset the sqSelected value.
+                        playerClicks = []  # Reset the playerClicks list.
+                    else:
+                        move = engine.Move(playerClicks[0], playerClicks[1], gs.board)
+                        print(move.getChessNotation())
+                        gs.makeMove(move)
+                        sqSelected = ()
+                        playerClicks = []
+
             drawGameState(screen, gs)
             clock.tick(MAX_FPS)
             p.display.flip()
@@ -40,7 +62,7 @@ def drawGameState(screen, gs):
 #Draw the squares on the board
 def drawBoard(screen):
     colors = [p.Color("white"), p.Color("gray")]
-    # colors = [p.Color("white"), p.Color("lightgreen")]
+    # colors = [p.Color("white"), p.Color("brown")]
 
     for r in range(DIMENSION):
         for c in range(DIMENSION):
